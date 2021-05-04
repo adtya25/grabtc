97abeb0ca20e8bd53fcbaee926f4b0b17e3ac137
adtya25 <79787334+adtya25@users.noreply.github.com>
2021-05-04 08:05:52 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,12 @@
+<?php
+/* Untuk Menjalankan Scrip
+pkg install php
+pkg install imagemagick
+*/
+$UA = "Mozilla/5.0 (Linux; Android 10; SM-A205F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.91 Mobile Safari/537.36";
+
+$COOKIE = "cf_clearance=9152afe8df884a68f21c9c88a1ec6e12c6e1d2f6-1620036122-0-150;_cfduid=dd0eb02c5dc8a53db28d35b18d9fd36571620036122;_ga=GA1.2.2028750964.1620036134;_gid=GA1.2.1944358361.1620036134;HstCfa4416931=1620036134528;HstCmu4416931=1620036134528;HstCnv4416931=1;HstCns4416931=1;_dtsu=104016038270397261D2607697662424;SesHashKey=rpej0w8m1eg62xxl;SesToken=ses_id%3D207429%26ses_key%3Drpej0w8m1eg62xxl;AccExist=207429;hideouttv=kontolodon;HstCla4416931=1620036188447;HstPn4416931=3;HstPt4416931=3";
+
+//URL SOLVEMEDIA PAPI CHALLENG
+
+$URL = "https://api-secure.solvemedia.com/papi/_challenge.js?k=pqPc5Z5GaUG3ibGIqqrhKMYliFDt3hgK;f=_ACPuzzleUtil.callbacks%5B0%5D;l=en;t=img;s=standard;c=js,h5c,h5ct,svg,h5v,v/h264,v/webm,h5a,a/mp3,a/ogg,ua/chrome,ua/chrome90,os/android,os/android10,fwv/BPqitg.bfqy44,jslib/jquery,htmlplus;am=.MYHfx5LXSH4PKkEHktdIQ;ca=ajax;ts=1620035283;ct=1620036126;th=white;r=0.8115378765867693";
